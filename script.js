068616499bd506016adf9e9829dd88253c26639a
Sumitkumar1109 <139613858+Sumitkumar1109@users.noreply.github.com>
2024-09-09 18:33:28 +0530

    micro-project-3

---
+++ script.js
@@ -0,0 +1,50 @@
+const keys=document.querySelectorAll('.item');
+let input=document.getElementById('calcInput');
+let disp=[];
+
+keys.forEach(key=>{
+    key.addEventListener('click',()=>{
+        if(disp.length===0 && (['+','/','*','isequal'].includes(key.id))){
+            disp[0]='INVALID INPUT';
+            input.value='INVALID INPUT';
+        }else{
+            if((input.value==='INVALID INPUT' || input.value==='EXPRESSION ERROR') ){
+                disp=[];
+                input.value='';
+            } 
+            if (key.classList.contains('num')) {
+                disp.push(key.textContent);
+            } else if(key.id==='delete'){
+                disp.pop();
+            } else if(key.id==='reset'){
+                disp.splice(0,disp.length);
+            } else if(key.id==='isequal'){
+                try{
+                    let result=eval(disp.join('').replace(/x/g,'*'));
+                    disp=(Array.from(result.toString()));
+                } catch{
+                    disp.splice(0,disp.length,'EXPRESSION ERROR');
+                }
+            } else if(key.classList.contains('op')){
+                lastElement=disp[(disp.length)-1];
+                if(['+','/','x','-'].includes(lastElement) && ['+','/','*'].includes(key.id)){
+                    disp[disp.length-1]=(key.textContent);
+                } else if (['-'].includes(key.id)){ 
+                    if (['+','-'].includes(lastElement)){
+                        disp[disp.length-1]=key.textContent;
+                    } else {
+                        disp.push(key.textContent);
+                    }
+                }else {
+                    disp.push(key.textContent);
+                }
+            } else if (key.id==='.' && disp[disp.length-1]==='.'){
+                disp[disp.length-1]===key.textContent;
+            } else {
+                disp.push(key.textContent);
+            }
+        }
+        
+        input.value=disp.join('');
+    })
+})
