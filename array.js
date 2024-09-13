aecdb8478139265bb9bf22e4b4f9d50c8064f12c
Sumit-1109 <sumitkumar2000sep@gmail.com>
2024-09-13 21:06:20 +0530

    Update array.js
    Updated operator handling.

--- array.js
+++ array.js
@@ -1,59 +1,48 @@
-const keys = document.querySelectorAll('.item');        
-let input = document.getElementById('calcInput');       
-let disp = [];                                          
+const keys=document.querySelectorAll('.item');
+let input=document.getElementById('calcInput');
+let disp=[];
 
-
-keys.forEach(key => {
-    key.addEventListener('click', () => {
-
-        if (disp.length === 0 && (['+', '/', '*', 'isequal'].includes(key.id))) {
-            disp[0] = 'INVALID INPUT';
-            input.value = 'INVALID INPUT';
-        } else {
-
-
-            if ((input.value === 'INVALID INPUT' || input.value === 'EXPRESSION ERROR')) {
-                disp = [];
-                input.value = '';
-            }
-
+keys.forEach(key=>{
+    key.addEventListener('click',()=>{
+        if(disp.length===0 && (['+','/','*','isequal'].includes(key.id))){
+            disp[0]='INVALID INPUT';
+            input.value='INVALID INPUT';
+        }else{
+            if((input.value==='INVALID INPUT' || input.value==='EXPRESSION ERROR') ){
+                disp=[];
+                input.value='';
+            } 
             if (key.classList.contains('num')) {
                 disp.push(key.textContent);
-            } else if (key.id === 'delete') {
+            } else if(key.id==='delete'){
                 disp.pop();
-
-            } else if (key.id === 'reset') {
-                disp.splice(0, disp.length);
-
-            } else if (key.id === 'isequal') {
-                try {
-                    let result = eval(disp.join('').replace(/x/g, '*'));
-                    result = parseFloat(result.toFixed(3));           
-                    disp = (Array.from(result.toString()));              
-
-                } catch {
-                    disp.splice(0, disp.length, 'EXPRESSION ERROR');       
+            } else if(key.id==='reset'){
+                disp.splice(0,disp.length);
+            } else if(key.id==='isequal'){
+                try{
+                    let result=eval(disp.join('').replace(/x/g,'*'));
+                    result=parseFloat(result.toFixed(3));
+                    disp=(Array.from(result.toString()));
+                } catch{
+                    disp.splice(0,disp.length,'EXPRESSION ERROR');
                 }
-
-            } else if (key.classList.contains('op')) {
-                let lastElement = disp[(disp.length) - 1];
-                let secondLastElement = disp[(disp.length) - 2];
-
-                
-                if (['+', '/', 'x', '-'].includes(lastElement)) {
-                    if (['x', '/'].includes(secondLastElement) && lastElement === '-') {
-                        if (['+', '/', 'x'].includes(key.id)) {
-                            disp.pop();
-                            disp.pop();
-                            disp.push(key.textContent);
-                        }
-                    } else if (['+', '/', 'x'].includes(key.id)) {
+            } else if(key.classList.contains('op')){
+                lastElement=disp[(disp.length)-1];
+                secondlastElement=disp[(disp.length-2)];
+                if (['/','x'].includes(secondlastElement)){
+                    if (lastElement==='-'){
+                        disp.pop();
                         disp.pop();
                         disp.push(key.textContent);
-                    } else if (key.id === '-') {
-                        if (lastElement !== '-') {
-                            disp.push(key.textContent);
-                        }
+                    }
+                }
+                if(['+','/','x','-'].includes(lastElement) && ['+','/','*'].includes(key.id)){
+                    disp[disp.length-1]=(key.textContent);
+                } else if (['-'].includes(key.id)){ 
+                    if (['+','-'].includes(lastElement)){
+                        disp[disp.length-1]=key.textContent;
+                    } else {
+                        disp.push(key.textContent);
                     }
                 } else {
                     disp.push(key.textContent);
@@ -67,7 +56,7 @@ keys.forEach(key => {
                 disp.push(key.textContent);
             }
         }
-
-        input.value = disp.join('');
+        
+        input.value=disp.join('');
     })
-});
+})
